3cc0d511d1477c58e8418ef84ebef74e3af8ef28
Tron <achuan-2@outlook.com>
2025-12-03 17:35:20 +0800

    🎨清理代码

--- src/api.ts
+++ src/api.ts
@@ -997,64 +997,6 @@ export async function ensureProjectDataFile(): Promise<void> {
 
 
 
-// **************************************** Riff (闪卡) ****************************************
-
-export async function addRiffCards(blockIDs: string[], deckID: string = Constants.QUICK_DECK_ID): Promise<any> {
-    let data = {
-        deckID: deckID,
-        blockIDs: blockIDs
-    };
-    let url = '/api/riff/addRiffCards';
-    return request(url, data);
-}
-
-export async function removeRiffCards(blockIDs: string[], deckID: string = Constants.QUICK_DECK_ID): Promise<any> {
-    let data = {
-        deckID: deckID,
-        blockIDs: blockIDs
-    };
-    let url = '/api/riff/removeRiffCards';
-    return request(url, data);
-}
-
-export async function getRiffDecks(): Promise<any> {
-    let url = '/api/riff/getRiffDecks';
-    return request(url, {});
-}
-
-export async function createRiffDeck(name: string): Promise<any> {
-    let data = {
-        name: name
-    };
-    let url = '/api/riff/createRiffDeck';
-    return request(url, data);
-}
-
-export async function removeRiffDeck(deckID: string): Promise<any> {
-    let data = {
-        deckID: deckID
-    };
-    let url = '/api/riff/removeRiffDeck';
-    return request(url, data);
-}
-
-export async function renameRiffDeck(deckID: string, name: string): Promise<any> {
-    let data = {
-        deckID: deckID,
-        name: name
-    };
-    let url = '/api/riff/renameRiffDeck';
-    return request(url, data);
-}
-
-export async function getRiffCards(deckID: string): Promise<any> {
-    let data = {
-        deckID: deckID
-    };
-    let url = '/api/riff/getRiffCards';
-    return request(url, data);
-}
-
 // **************************************** Habit Management API ****************************************
 
 export async function writeHabitData(data: any): Promise<any> {
--- src/components/HabitCheckInEmojiDialog.ts
+++ src/components/HabitCheckInEmojiDialog.ts
@@ -183,7 +183,7 @@ export class HabitCheckInEmojiDialog {
                 this.sharedPicker = new Picker({
                     i18n: zh_CN,
                     locale: 'zh_CN',
-                    dataSource: 'http://127.0.0.1:6806/plugins/siyuan-plugin-task-note-management/assets/emojis_search.json'
+                    dataSource: window.siyuan.config.system.dataDir+'/plugins/siyuan-plugin-task-note-management/assets/emojis_search.json'
                 }
                 ) as Picker;
             } catch (e) {
